51b3cc6c9e21374683430b2c1a121c3732951214
Ahmet Safa Orhan <ahmetsafaorhan@gmail.com>
2015-08-25 18:16:26 +0300

    Start adding multi language support
    - Add js.cookie.js
    - Add lang.js
    - Add uiData.json

---
+++ js/lang.js
@@ -0,0 +1,22 @@
+function setLanguage (l) {
+	Cookies.set('lang', l);
+	setupUi();
+}
+
+function setupUi () {
+	var lang = Cookies.get('lang');
+
+	if(!lang) {
+		lang = "en";
+	}
+
+	var uiText = JSON.parse(uiData);
+
+
+	$(".multilang").each(function(index, value){
+		var textId = $(this).attr('textId');
+		$(this).text(uiText[textId][lang]);
+	});
+
+
+}
